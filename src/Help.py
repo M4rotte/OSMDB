7cc82814aedd46d4b71f217986d4377080356df7
Stef <stef@oxyure.com>
2018-07-20 23:09:30 +0200

    Add help for "get parameter"

--- src/Help.py
+++ src/Help.py
@@ -23,6 +23,7 @@ def helpAndExit(this):
     elif this == 'tag-hosts':          print("""Usage:\n {} --set-tag <tag> <query>
  {} --set-tag <tag> --selection <query> --tags <tag query>""".format(name,name), file=sys.stderr)
     elif this == 'set-param':          print("""Usage: {} --set-param <hostname|domain|*> <param>=<value>""".format(name), file=sys.stderr)
+    elif this == 'get-param':          print("""Usage: {} --get-param <parameter> <hostname|domain>""".format(name), file=sys.stderr)
     sys.exit(99)
 
 
